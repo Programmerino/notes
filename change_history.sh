e8be186d29facff17eac9b4fb968926a8b732491
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2023-02-07 22:23:25 -0700

    February  6, 2023

--- change_history.sh
+++ change_history.sh
@@ -1,8 +1,8 @@
-grep -rlw --include="*.md" -e "#protected" . >> .gitignore
+grep -rlw --include="*.md" -e "#protected" * >> .gitignore
 awk -i inplace '!seen[$0]++' .gitignore
-git ls-files --others -i --exclude-standard > /tmp/ignored
 git add .
 git rm -r --cached .
 git add .
+git ls-files --others -i --exclude-standard > /tmp/ignored
 git commit -m "$(date -d yesterday '+%B %e, %Y')"
 git filter-repo --invert-paths --paths-from-file /tmp/ignored --force --debug
--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@ shopt -s dotglob
 sudo umount /home/davis/Documents/Obsidian/ObsidianVault/storage
 mv /home/davis/Documents/Obsidian/ObsidianVault/storage/* /home/davis/Zotero/storage/
 sudo mount --bind /home/davis/Zotero/storage/ "/home/davis/Documents/Obsidian/ObsidianVault/storage"
-grep -rlw --include="*.md" -e "#protected" . >> .gitignore
+grep -rlw --include="*.md" -e "#protected" * >> .gitignore
 awk -i inplace '!seen[$0]++' .gitignore
-git ls-files --others -i --exclude-standard > /tmp/ignored
 git add .
 git rm -r --cached .
 git add .
+git ls-files --others -i --exclude-standard > /tmp/ignored
 echo Starting cacheBuild
 cacheBuild
 echo cacheBuild done
