551b3c9de42af34e75de5018098b7c5dcf20cadd
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2023-02-15 10:38:35 -0700

    February 14, 2023

--- change_history.sh
+++ change_history.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p python3Packages.git-filter-repo
 git log --all --pretty=format: --name-only --diff-filter=A > /tmp/test
 cat /tmp/test | sed '/^[[:space:]]*$/d' | git check-ignore --no-index --stdin >> .gitignore
 grep -rlw --include="*.md" -e "#protected" * >> .gitignore
